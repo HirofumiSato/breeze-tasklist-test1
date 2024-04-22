b8f9592ffca2266ed7728719e0f98202c8a79c68
HirofumiSato <midnight.8718@gmail.com>
2024-04-22 15:08:58 +0900

    ダッシュボードにユーザー一覧の表示

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,13 +5,34 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
+    <div class="container max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <div class="content">
+            <table class="shadow-lg border-separate w-full">
+                <thead class="bg-blue-100">
+                    <tr>
+                    <th class="bg-blue-100 border text-left px-8 py-4">
+                        Name
+                    </th>
+                    <th class="bg-blue-100 border text-left px-8 py-4">
+                        Email
+                    </th>
+                    <!-- Add more table headers for other user data -->
+                    </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200">
+                    @foreach($users as $user)
+                    <tr>
+                    <td class="px-6 py-4 whitespace-nowrap border-b border-gray-300">
+                        {{ $user->name }}
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap border-b border-gray-300">
+                        {{ $user->email }}
+                    </td>
+                    <!-- Add more table cells for other user data -->
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </x-app-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    mode: 'jit', //ここを追加
+
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
